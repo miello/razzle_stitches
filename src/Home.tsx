c6030a27f1f6301f43b427b392639e3878d3a817
miello <partearth1212@gmail.com>
2021-08-10 08:35:26 +0700

    first commit

---
+++ src/Home.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import logo from "./react.svg";
+import { styled } from "@stitches/react";
+import "./Home.css";
+
+const Button = styled("button", {
+  backgroundColor: "red",
+});
+
+class Home extends React.Component<{}, {}> {
+  public render() {
+    return (
+      <div className="Home">
+        <div className="Home-header">
+          <img src={logo} className="Home-logo" alt="logo" />
+          <h2>Welcome to Razzle</h2>
+        </div>
+        <Button>Hello World</Button>
+        <p className="Home-intro">
+          To get started, edit <code>src/App.tsx</code> or{" "}
+          <code>src/Home.tsx</code> and save to reload.
+        </p>
+        <ul className="Home-resources">
+          <li>
+            <a href="https://github.com/jaredpalmer/razzle">Docs</a>
+          </li>
+          <li>
+            <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
+          </li>
+          <li>
+            <a href="https://palmer.chat">Community Slack</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Home;
